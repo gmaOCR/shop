ac52cc5543278090130ca32b1a41c322502366d0
greg <marianigregory@pm.me>
2024-10-03 09:07:39 +0200

    adding assets

---
+++ frontend/src/datas/candles.js
@@ -0,0 +1,29 @@
+import candle1 from '../assets/candle_1.jpg'
+import candle2 from '../assets/candle_2.jpg'
+import candle3 from '../assets/candle_3.jpg'
+
+export const candelsList = [
+  {
+    id: '001',
+    name: 'Candle 1',
+    description: 'A beautiful candle not so expensive',
+    price: 30,
+    image: candle1,
+  },
+  {
+    id: '002',
+    name: 'Candle 2',
+    description: 'A beautiful candle',
+    price: 40,
+    image: candle2,
+  },
+  {
+    id: '003',
+    name: 'Candle 23',
+    description: 'A beautiful candle not so cheap',
+    price: 400,
+    image: candle3,
+  },
+]
+
+export default candelsList
