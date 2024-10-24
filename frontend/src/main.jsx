23ae3d326a7e6d63f0752158174550c5daa94026
greg <marianigregory@pm.me>
2024-10-24 09:53:10 +0200

    providers added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App.jsx'
 import { CartProvider } from '@/components/context/CartContext'
+import { ProductProvider } from '@/components/context/ProductsContext.jsx'
 import './index.css'
 
 const queryClient = new QueryClient()
@@ -12,7 +13,9 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <CartProvider>
-        <App />
+        <ProductProvider>
+          <App />
+        </ProductProvider>
       </CartProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
